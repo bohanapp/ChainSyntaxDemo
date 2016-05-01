2f4aad316629367d38049d2a536ed67fab3f8113
高渊 <gaoyuanyzu@gmail.com>
2016-05-01 21:12:13 +0800

    Initial Commit

---
+++ ChainSyntaxDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ChainSyntaxDemo
+//
+//  Created by gaoyuan on 5/1/16.
+//  Copyright © 2016 gaoyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ChainSyntaxDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChainSyntaxDemo
+//
+//  Created by gaoyuan on 5/1/16.
+//  Copyright © 2016 gaoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
